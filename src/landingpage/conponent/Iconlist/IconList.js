94ce4a24bfe3c338dee6aa9a8ecfc140bf8092ca
IbrarGill <105701207+IbrarGill@users.noreply.github.com>
2022-06-06 10:47:53 +0500

    ui changes 6/6/22

--- src/landingpage/conponent/Iconlist/IconList.js
+++ src/landingpage/conponent/Iconlist/IconList.js
@@ -12,11 +12,11 @@ function IconList() {
     <div id='iconslist'>
       <center>
         <div className='row'>
-          <img className='col-2  mx-auto my-2 py-auto' src={icon1} alt='icon1' width={30} height={60} />
-          <img className='col-2  mx-auto my-2 py-auto' src={icon2} alt='icon2' width={30} height={60} />
-          <img className='col-2  mx-auto my-2 py-auto' src={icon3} alt='icon3' width={30} height={60} />
-          <img className='col-2  mx-auto my-2 py-auto' src={icon4} alt='icon4' width={30} height={60} />
-          <img className='col-2  mx-auto my-2 py-auto' src={icon5} alt='icon5' width={30} height={60} />
+          <img className='col-2 imgeffect mx-auto my-2 py-auto' src={icon1} alt='icon1' width={30} height={60} />
+          <img className='col-2 imgeffect mx-auto my-2 py-auto' src={icon2} alt='icon2' width={30} height={60} />
+          <img className='col-2 imgeffect mx-auto my-2 py-auto' src={icon3} alt='icon3' width={30} height={60} />
+          <img className='col-2 imgeffect mx-auto my-2 py-auto' src={icon4} alt='icon4' width={30} height={60} />
+          <img className='col-2 imgeffect mx-auto my-2 py-auto' src={icon5} alt='icon5' width={30} height={60} />
         </div>
       </center>
 
--- src/landingpage/conponent/details_cards/detailcard.js
+++ src/landingpage/conponent/details_cards/detailcard.js
@@ -10,7 +10,7 @@ function DetailsCard() {
 
             <div className="grid-container-cards py-5 mx-5">
                 <div className='rid-item-card  border-end border-bottom border-3 '>
-                    <div className="m-3">
+                    <div className="m-3 cards">
                         <img src={hub} alt='hub' />
                         <div className='fw-bold fs-6 my-2  ' style={{ color: '#022A7D' }}>Design your User Profile to fit your style. </div>
                         <div className='my-2 ' style={{ color: '#022A7D' }}>Access your accounts activity and
@@ -22,7 +22,7 @@ function DetailsCard() {
 
                 </div>
                 <div className="rid-item-card  border-end border-bottom border-3">
-                    <div className="m-3">
+                    <div className="m-3 cards">
                         <img src={rocket} alt='hub' />
                         <div className='fw-bold fs-6 my-2' style={{ color: '#022A7D' }}>Design your User Profile to fit your style. </div>
                         <span className='my-2 ' style={{ color: '#022A7D' }}>Access your accounts activity and
@@ -33,7 +33,7 @@ function DetailsCard() {
 
                 </div>
                 <div className="rid-item-card  border-bottom border-3">
-                    <div className="m-3">
+                    <div className="m-3 cards">
                         <img src={settings} alt='hub' />
                         <div className='fw-bold fs-6 my-2' style={{ color: '#022A7D' }}>Design your User Profile to fit your style. </div>
                         <span className='my-2 ' style={{ color: '#022A7D' }}>Access your accounts activity and
@@ -45,7 +45,7 @@ function DetailsCard() {
 
                 </div>
                 <div className="rid-item-card  border-end  border-3">
-                    <div className="m-3">
+                    <div className="m-3 cards">
                         <img src={hub} alt='hub' />
                         <div className='fw-bold fs-6 my-2' style={{ color: '#022A7D' }}>Design your User Profile to fit your style. </div>
                         <span className='my-2 ' style={{ color: '#022A7D' }}>Access your accounts activity and
@@ -58,7 +58,7 @@ function DetailsCard() {
 
                 </div>
                 <div className="rid-item-card  border-end border-3">
-                    <div className="m-3">
+                    <div className="m-3 cards">
                         <img src={rocket} alt='hub' />
                         <div className='fw-bold fs-6 my-2' style={{ color: '#022A7D' }}>Design your User Profile to fit your style. </div>
                         <span className='my-2 ' style={{ color: '#022A7D' }}>Access your accounts activity and
@@ -69,7 +69,7 @@ function DetailsCard() {
 
                 </div>
                 <div className="rid-item-card ">
-                    <div className="m-3">
+                    <div className="m-3 cards">
                         <img src={settings} alt='hub' />
                         <div className='fw-bold fs-6 my-2' style={{ color: '#022A7D' }}>Design your User Profile to fit your style. </div>
                         <span className='my-2 '  style={{ color: '#022A7D' }}>Access your accounts activity and
@@ -82,59 +82,6 @@ function DetailsCard() {
             </div>
 
 
-            {/* <div className='row justify-content-center'>
-                <div className='col-12 col-md-6 col-sm-12 col-lg-3  px-4 '>
-                    <img src={hub} alt='hub' />
-                    <div className='fw-bold fs-4 ' style={{ color: '#022A7D' }}>Design your User Profile to fit your style. </div>
-                    <span  style={{ color: '#022A7D' }}>Access your accounts activity and
-                        transactions from here. You can
-                        also control the collections that are
-                        under your influence.</span>
-                </div>
-                <div className='col-12 col-md-6 col-sm-12 col-lg-3  px-4 border-end  border-bottom border-3 py-5 border-#508CD3'>
-                    <img src={rocket} alt='hub' />
-                    <div className='fw-bold fs-4' style={{ color: '#022A7D' }}>Design your User Profile to fit your style. </div>
-                    <span  style={{ color: '#022A7D' }}>Access your accounts activity and
-                        transactions from here. You can
-                        also control the collections that are
-                        under your influence.</span>
-                </div>
-                <div className='col-12 col-md-6 col-sm-12 col-lg-3  px-4  border-bottom border-3 py-5 border-#508CD3'>
-                    <img src={settings} alt='hub' />
-                    <div className='fw-bold fs-4' style={{ color: '#022A7D' }}>Design your User Profile to fit your style. </div>
-                    <span  style={{ color: '#022A7D' }}>Access your accounts activity and
-                        transactions from here. You can
-                        also control the collections that are
-                        under your influence.</span>
-                </div>
-            </div>
-
-            <div className='row justify-content-center'>
-                <div className='col-12 col-md-6 col-sm-12 col-lg-3 px-4 border-end py-5 border-3 border-#508CD3'>
-                    <img src={hub} alt='hub' />
-                    <div className='fw-bold fs-4' style={{ color: '#022A7D' }}>Design your User Profile to fit your style. </div>
-                    <span  style={{ color: '#022A7D' }}>Access your accounts activity and
-                        transactions from here. You can
-                        also control the collections that are
-                        under your influence.</span>
-                </div>
-                <div className='col-12 col-md-6 col-sm-12 col-lg-3 px-4 border-end py-5 border-3 border-#508CD3'>
-                    <img src={rocket} alt='hub' />
-                    <div className='fw-bold fs-4' style={{ color: '#022A7D' }}>Design your User Profile to fit your style. </div>
-                    <span  style={{ color: '#022A7D' }}>Access your accounts activity and
-                        transactions from here. You can
-                        also control the collections that are
-                        under your influence.</span>
-                </div>
-                <div className='col-12 col-md-6 col-sm-12 col-lg-3 px-4 py-5'>
-                    <img src={settings} alt='hub' />
-                    <div className='fw-bold fs-4' style={{ color: '#022A7D' }}>Design your User Profile to fit your style. </div>
-                    <span  style={{ color: '#022A7D' }}>Access your accounts activity and
-                        transactions from here. You can
-                        also control the collections that are
-                        under your influence.</span>
-                </div>
-            </div> */}
         </div>
     )
 }
